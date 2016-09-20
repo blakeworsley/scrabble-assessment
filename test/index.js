c82ca2d3c97b6e8b1d8837acc681a38631881036
Blake Worsley <blakeworsley@gmail.com>
2016-09-19 22:07:09 -0600

    tests and basic functionality working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,34 @@
 const assert = require('chai').assert;
+const ScoreGame = require('../lib/components/ScoreGame');
 
 describe('our test bundle', function() {
   it('should work', function(){
-    assert.isFunction(createNewContact);
+    var scoreGame = new ScoreGame();
+    assert.isFunction(scoreGame.scoreWord);
+  });
+  it('returns a score of 8 for the word hello', function(){
+    var scoreGame = new ScoreGame();
+    var score = scoreGame.scoreWord('hello');
+    assert.equal(score, 8);
+  });
+  it('should return 0 if scoreWord is given an empty string or null', function(){
+    var scoreGame = new ScoreGame();
+    var score = scoreGame.scoreWord('');
+    assert.equal(score, 0);
+  });
+  it('should remove any whitespace before or after any given word', function(){
+    var scoreGame = new ScoreGame();
+    var score = scoreGame.scoreWord('   h e l lo ');
+    assert.equal(score, 8);
+  });
+  it('should not count number if entered into the input', function(){
+    var scoreGame = new ScoreGame();
+    var score = scoreGame.scoreWord('  232 h e 34l l34o ');
+    assert.equal(score, 8);
+  });
+  it('should take a second argument that multiplies the score', function(){
+    var scoreGame = new ScoreGame();
+    var score = scoreGame.scoreWord('hel12893921lo', 2);
+    assert.equal(score, 16);
   });
 });
