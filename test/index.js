95d6e8ffb5d8fbf4a1ce4067e414743e79ec4bc4
Blake Worsley <blakeworsley@gmail.com>
2016-09-20 13:53:06 -0600

    finished assessment

--- test/index.js
+++ test/index.js
@@ -1,34 +1,28 @@
 const assert = require('chai').assert;
-const ScoreGame = require('../lib/components/ScoreGame');
+const scoreWord = require('../lib/components/ScoreGame');
 
 describe('our test bundle', function() {
   it('should work', function(){
-    var scoreGame = new ScoreGame();
-    assert.isFunction(scoreGame.scoreWord);
+    assert.isFunction(scoreWord);
   });
   it('returns a score of 8 for the word hello', function(){
-    var scoreGame = new ScoreGame();
-    var score = scoreGame.scoreWord('hello');
+    var score = scoreWord('hello');
     assert.equal(score, 8);
   });
   it('should return 0 if scoreWord is given an empty string or null', function(){
-    var scoreGame = new ScoreGame();
-    var score = scoreGame.scoreWord('');
+    var score = scoreWord('');
     assert.equal(score, 0);
   });
   it('should remove any whitespace before or after any given word', function(){
-    var scoreGame = new ScoreGame();
-    var score = scoreGame.scoreWord('   h e l lo ');
+    var score = scoreWord('   h e l lo ');
     assert.equal(score, 8);
   });
   it('should not count number if entered into the input', function(){
-    var scoreGame = new ScoreGame();
-    var score = scoreGame.scoreWord('  232 h e 34l l34o ');
+    var score = scoreWord('  232 h e 34l l34o ');
     assert.equal(score, 8);
   });
   it('should take a second argument that multiplies the score', function(){
-    var scoreGame = new ScoreGame();
-    var score = scoreGame.scoreWord('hel12893921lo', 2);
+    var score = scoreWord('hel12893921lo', 2);
     assert.equal(score, 16);
   });
 });
